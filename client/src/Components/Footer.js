ad4446446299149ad8ad5ed462075cf47f7577a0
oaluna <oscarluna.webdev@gmail.com>
2021-09-29 14:30:24 -0700

    Begin separating styled JSX from footer component to its own CSS file

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,35 +1,22 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.svg";
-
-const Footer = () => (
-  <footer
-    style={{
-      position: "absolute",
-      left: "0vw",
-      right: "0vw",
-      bottom: "0vh",
-      padding: 0,
-      width: "100%",
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-evenly",
-      background: "purple",
-    }}
-  >
-    <div className="footerLogo">
-      <img
-        src={Logo}
-        alt="site logo"
-        style={{ textAlign: "left", width: "100px", margin: "15px auto" }}
-      />
-    </div>
-    <div className="footerCopyright">
-      <small>
-        &copy;2021 by <Link to="https://oscarluna.dev">Oscar Armando Luna</Link>
-      </small>
-    </div>
-  </footer>
-);
-
+import "./Footer.css";
+function Footer () {
+  return (
+    <footer className="footer">
+      <div className="footerLogo">
+        <img
+          src={Logo}
+          alt="site logo"
+          style={{ textAlign: "left", width: "100px", margin: "15px auto" }}
+        />
+      </div>
+      <div className="footerCopyright">
+        <small>
+          &copy;2021 by <Link to="https://oscarluna.dev">Oscar Armando Luna</Link>
+        </small>
+      </div>
+    </footer>
+  );
+}
 export default Footer;
