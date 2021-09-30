a538abc21f97d0327cb254229e35cc8e46439980
oaluna <oscarluna.webdev@gmail.com>
2021-09-30 14:12:43 -0700

    Fixed DB connection issue in server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose"); // new
+const mongoose = require("mongoose");
+const config = require("./config/Config");
 
-const mongoUri = process.env.DB;
-const port = process.env.PORT || 5000
+const app = express();
 
-// Connect to MongoDB database
+app.use(express.json());
+
+const mongoUri =
+  process.env.MONGO_URI || config.mongoUri ||
+  "mongodb://dbLuna:QJkCJ11LKg9QklFj@cluster0-shard-00-01.gzj6j.azure.mongodb.net:27017/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&directConnection=true&ssl=true";
+
 mongoose
-  .connect(mongoUri, { useUnifiedTopology: false, useNewUrlParser: true })
-    .then(() => {
-      console.log("MongoDB connection established")
-}).catch((err) => console.log(err, "Failed to connect"))
+  .connect(mongoUri)
+  .then(() => console.log("💻 MongoDB Connected"))
+  .catch(err => console.error(err));
 
-const app = express(app);
-
-app.listen(5000, () => {
-  console.log("Server has started! Listening from port " + port);
+app.get("/", (req, res) => {
+  res.send("Server working 🔥");
 });
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => `Server running on port port 🔥`);
